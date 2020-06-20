dcc80109848d536d9734e732d08c4cd0dbb8a8c6
Léo Serrão Barbosa <leo_serrao@hotmail.com>
2020-06-19 21:25:22 -0300

    Primeiro commit

---
+++ aula-01/index.js
@@ -0,0 +1,18 @@
+let entrada = prompt('Insira um valor: ')
+
+
+let operador = 100
+
+let operando = 50
+
+let resultado = operador * operando
+
+console.log(resultado)
+
+let nome = "Léo"
+
+let sobrenome = "Barbosa"
+
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto)
---
+++ aula-02/index.js
@@ -0,0 +1,8 @@
+var nome = 'Leon';
+
+if (nome === 'Leo') {
+    console.log("Legal esse é o seu nome mesmo!");
+} else {
+    console.log('Que pena, seu nome não é Leo');
+}
+
